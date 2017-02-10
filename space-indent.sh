3209cc81f96365c68765b2d2baa7005de6a02add
Moyu <492899414@qq.com>
2017-02-10 15:52:18 +0800

    butter space-indent.sh

--- space-indent.sh
+++ space-indent.sh
@@ -1,29 +1,50 @@
 #!/bin/sh
+# scriptname [...files] [from space number] [to space number]
+
+
 is_number() {
     # return $?   0 -> true
     [ "$1" -eq "$1" ] 2>/dev/null
 }
 
+repeat() {
+    local NUM=$2
+    local STR=""
+    while [ $NUM -gt 0 ]; do
+        STR+="$1"
+        NUM=$[$NUM-1]
+    done
+    return_val="$STR"
+}
+return_val=""
 SPNUM=4
 _last=${!#}
+second_to_last="${@:(-2):1}"
 
 for arg in $@; do
     args+=($arg)
 done
 
-if [ -n "$1" ] && is_number $1 ; then
-    SPNUM=$[$1]
+if [ -n "$_last" ] && is_number $_last ; then
+    SPNUM=$[$_last]
 else
     args+=("null")
 fi
 
-echo "Number => $SPNUM"
+repeat " " "$SPNUM"
+STR=$return_val
+echo "To Space Number => $SPNUM"
+unset "args[${#args[@]}-1]"
 
-while [ $SPNUM -gt 0 ]; do
-    STR+=" "
-    SPNUM=$[$SPNUM-1]
-done
+if [ -n "$second_to_last" ] && is_number $second_to_last ; then
+    unset "args[${#args[@]}-1]"
+    echo "FROM Space Number => $second_to_last"
+    repeat " " $second_to_last
+    FROM=$return_val
+    sed -i '' -e "s/^$FROM/$STR/g" "${args[@]}"
+fi
 
 tab=$'\t'
+sed -i '' -e "s/^$tab/$STR/g" "${args[@]}"
 
-sed -i '' -e "s/$tab/$STR/g" "${args[@]}"
+
