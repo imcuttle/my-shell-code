1d8f92708971f7cac38cc23a02bfbd93fa8aaee9
Moyu <492899414@qq.com>
2017-02-13 00:00:56 +0800

    from bash

--- project-init/proj-init.sh
+++ project-init/proj-init.sh
@@ -3,6 +3,10 @@ SELF_PATH="$(cd -P -- "$(dirname -- "$0")" && pwd -P)/$(basename -- "$0")"
 SELF_PATH="$(readlink "$SELF_PATH" || echo $SELF_PATH)"
 DIR_PATH="$(dirname "$SELF_PATH")"
 
+dir="."
+[ -n "$1" ] && dir="$1"
+[ ! -d "$dir" ] && mkdir "$dir"
+cd $dir
 sh "$DIR_PATH/copy-license.sh"
 cp -v "$DIR_PATH/.gitignore" .
 
